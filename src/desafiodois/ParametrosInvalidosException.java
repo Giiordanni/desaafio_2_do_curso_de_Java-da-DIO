b634ae633768c559bd56ff5ace07a6bf5dc820aa
giordanni <jordani.alformiga@gmail.com>
2024-06-17 14:20:37 -0300

    Update ParametrosInvalidosException.java

--- src/desafiodois/ParametrosInvalidosException.java
+++ src/desafiodois/ParametrosInvalidosException.java
@@ -3,13 +3,14 @@ package desafiodois;
 
 public class ParametrosInvalidosException extends Exception{
 
-	ParametrosInvalidosException(int num1,int num2) throws ParametrosInvalidosException{
-		
+	ParametrosInvalidosException(String message) {
+		super(message);
 	}
 	
-	@Override
-	public String toString() {
-		return "O segundo parâmetro deve ser maior que o primeiro";
+	
+	ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser maior que o primeiro");
 	}
+	
 
 }
